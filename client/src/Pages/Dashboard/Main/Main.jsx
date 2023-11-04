2dd401c01db5903e000159184247cc2a48ed1c59
AIProdigyHemant <141361950+AIProdigyHemant@users.noreply.github.com>
2023-11-05 02:12:42 +0530

    Delete client directory

--- client/src/Pages/Dashboard/Main/Main.jsx
+++
@@ -1,7 +0,0 @@
-import { React } from 'react'
-import './main.css'
-import Projects from '../../../Components/Projects/Projects'
-
-export default function Main ({ projects, loading }) {
-  return <Projects projects={projects} loading={loading} />
-}
